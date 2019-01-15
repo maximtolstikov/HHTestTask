5a78da05cef8e62c5d164b75a7eb1f99b30da3f5
Maxim Tolstikov <vadimovich@list.ru>
2019-01-15 18:22:51 +0300

    Сверстал AuthViewController.

---
+++ HHTestTask/App/Flows/Controllers/AuthViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AuthViewController.swift
+//  HHTestTask
+//
+//  Created by Maxim Tolstikov on 15/01/2019.
+//  Copyright © 2019 Maxim Tolstikov. All rights reserved.
+//
+
+import UIKit
+
+class AuthViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
